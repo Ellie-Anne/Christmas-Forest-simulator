2b2bdf975119ece9de4296164d9509c78c7f3a75
Ellie-Anne Watts <ellieanne.watts@gmail.com>
2020-12-17 14:48:59 +0000

    added female wolves to 5 years prior

--- Forest/Forest/Pack.cs
+++ Forest/Forest/Pack.cs
@@ -13,6 +13,12 @@ namespace Forest
             Wolf wolf;
             wolf = new Wolf(0, true);
             Wolves.Add(wolf);
+
+            for (int i = 0; i < 3; i++)
+            {
+                wolf = new Wolf(0, false);
+                Wolves.Add(wolf);
+            }
         }
     }
 }
